b34bb79e1c99ea2c6791800b4a5012938b8bd178
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:35:05 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,15 @@
+package HosGeldinMesajlari;
+
+public class JavaDeseni {
+
+	public static void main(String[] args) {
+        System.out.println("    JV    ");
+        System.out.println("   JAVA   ");
+        System.out.println("  JAVVVA  ");
+        System.out.println(" JAVVVVVA ");
+        System.out.println("  JAVVVA  ");
+        System.out.println("   JAVA   ");
+        System.out.println("    JV    ");	
+	}
+
+}
