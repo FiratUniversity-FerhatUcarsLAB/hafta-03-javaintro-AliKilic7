135e2e5dc0edd5d5e0e7b3796f4f05d94a7b7f79
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:40:41 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,24 @@
+package MetinveString;
+
+public class IsimKarti {
+
+	public static void main(String[] args) {
+		String ad = "Selahattin Ali";
+		String soyad = "Kılıç";
+		String ogrencino = "250541036";
+		String bolum = "Yazılım Mühendisliği";
+		System.out.println(" ___________________________");
+		System.out.println("|     Öğrenci Bilgileri     |");
+		System.out.println("|---------------------------|");
+		System.out.print("|Ad:         "+ad);
+		System.out.println(" |");
+		System.out.print("|Soyad:      "+soyad);
+		System.out.println("          |");
+		System.out.print("|Öğrenci NO.:"+ogrencino);
+		System.out.println("      |");
+		System.out.print("|Bölüm:"+bolum);
+		System.out.println(" |");
+		System.out.println("|___________________________|");
+	}
+
+}
