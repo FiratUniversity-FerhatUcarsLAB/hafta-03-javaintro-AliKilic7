44f65790c0a26c79bfe5357ad6c47eb0475075e5
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:40:02 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,15 @@
+package MetinveString;
+
+public class ASCIIArt {
+
+	public static void main(String[] args) {
+		System.out.println("      A        L           İ");
+		System.out.println("     A A       L            ");
+		System.out.println("    A   A      L           İ");
+		System.out.println("   AAAAAAA     L           İ");
+		System.out.println("  A       A    L           İ");
+		System.out.println(" A         A   L           İ");
+		System.out.println("A           A  LLLLLLLLLL  İ");
+	}
+
+}
