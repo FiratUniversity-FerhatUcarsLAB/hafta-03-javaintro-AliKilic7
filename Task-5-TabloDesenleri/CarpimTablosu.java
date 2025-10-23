dafb7f75f684ae57ffb762a019d7becf460c00ab
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:41:05 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,29 @@
+package KarmasıkCıktıFormatları;
+
+public class CarpımTablosu {
+
+	public static void main(String[] args) {
+		System.out.println(" ___________________________________________________________");
+		System.out.println("|                                                           |");
+		System.out.println("|                   2'NİN ÇARPIM TABLOSU                    |");
+		System.out.println("|___________________________________________________________|");
+		System.out.println("|     |     |     |     |     |     |     |     |     |     |");
+		System.out.println("|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  | 10  |");
+		System.out.println("|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|");
+		System.out.println("|     |     |     |     |     |     |     |     |     |     |");	
+		System.out.print("|  "+2*1);
+		System.out.print("  |  "+2*2);
+		System.out.print("  |  "+2*3);
+		System.out.print("  |  "+2*4);
+		System.out.print("  | "+2*5);
+		System.out.print("  | "+2*6);
+		System.out.print("  | "+2*7);
+		System.out.print("  | "+2*8);
+		System.out.print("  | "+2*9);
+		System.out.print("  | "+2*10);
+		System.out.println("  |  ");
+		System.out.println("|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|");	
+		
+	}
+
+}
