0cce87f81eb2ed58b51ebc4542654a55e33b6a3d
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:40:24 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,21 @@
+package MetinveString;
+
+public class HesapOzeti {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" __________________________________ ");
+		System.out.println("|                                  |");
+		System.out.println("|        Alışveriş Listesi         |");
+		System.out.println("|==================================|");
+		System.out.println("|1 Adet Mekanik Klavye:1000        |");
+		System.out.println("|----------------------------------|");
+		System.out.println("|1 Adet Kablosuz Mouse:500         |");
+		System.out.println("|----------------------------------|");
+		System.out.println("|1 Adet Kulak Üstü Kulaklık:3500   |");
+		System.out.println("|==================================|");
+		System.out.println("|          -Toplam:5000-           |");
+		System.out.println("|__________________________________|");
+	}
+
+}
