8911878fa9b47c2c37edf06e4c2042548e4f098f
AliKilic7 <250541036@firat.edu.tr>
2025-10-23 22:41:50 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,46 @@
+package KarmasıkCıktıFormatları;
+
+public class UsluSayıTablo {
+
+	public static void main(String[] args) {
+		int a1,a2,a3,a4,a5;
+		a1 = 1;
+		a2 = 2;
+		a3 = 3;
+		a4 = 4;
+		a5 = 5;
+		System.out.println(" _______________________ ");
+		System.out.println("|                       |");
+		System.out.println("|   ÜSLÜ SAYI TABLOSU   |");
+		System.out.println("|_______________________|");
+		
+		System.out.println("|   a   |  a^2  |  a^3  |");
+		System.out.println("|***********************|");
+		System.out.print("|   "+a1);
+		System.out.print("   |   "+a1*a1);
+		System.out.print("   |   "+a1*a1*a1);
+		System.out.println("   |");
+		System.out.println("|=======================|");
+		System.out.print("|   "+a2);
+		System.out.print("   |   "+a2*a2);
+		System.out.print("   |   "+a2*a2*a2);
+		System.out.println("   |");
+		System.out.println("|=======================|");
+		System.out.print("|   "+a3);
+		System.out.print("   |   "+a3*a3);
+		System.out.print("   |   "+a3*a3*a3);
+		System.out.println("  |");
+		System.out.println("|=======================|");
+		System.out.print("|   "+a4);
+		System.out.print("   |  "+a4*a4);
+		System.out.print("   |   "+a4*a4*a4);
+		System.out.println("  |   ");
+		System.out.println("|=======================|");
+		System.out.print("|   "+a5);
+		System.out.print("   |   "+a5*a5);
+		System.out.print("  |   "+a5*a5*a5);
+		System.out.println(" |");
+		System.out.println("|_______________________|");
+	}
+
+}
